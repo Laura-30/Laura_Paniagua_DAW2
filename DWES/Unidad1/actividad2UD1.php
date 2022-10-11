d4f70e6d652e6ae21aac3f2e6712953a432621e2
Laura-30 <laura.paniagua@cesjuanpablosegundo.es>
2022-10-11 18:28:18 +0200

    update

---
+++ DWES/Unidad1/actividad2UD1.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+  <title>Activdad 2</title>
+</head>
+
+<body>
+
+  <?php
+  
+  echo "<h1> Práctica 2 </h1>";
+  echo "<p>Este código HTML ha sido generado mediante instrucciones escritas en lenguaje PHP.</p>";
+  
+
+  ?>
+
+</body>
+
+</html>
---
+++ DWES/Unidad1/actividad3UD1.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+  <title>Actividad 3</title>
+</head>
+
+<body>
+
+  <?php
+  
+  echo "<h1>Manejo de cadenas </h1>";
+  echo "<h2>Ejemplo 2 </h2>";
+  print ("<UL>\n");
+  print ("<LI>uno</LI>\n");
+  print ("<LI>dos</LI>\n");
+  print ("<LI>tres</LI>\n");
+  print ("<LI>cuatro</LI>\n");
+  print ("<LI>cinco</LI>\n");
+  print ("</UL>\n");
+  ?>
+
+</body>
+
+</html>
